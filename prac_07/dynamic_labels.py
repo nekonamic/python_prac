602d586d6fb211cc07f74e959bce7a5991d7d0d3
Emiria <Emiriachan@users.noreply.github.com>
2021-11-03 15:49:20 +0800

    completed prac_07

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -1,12 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
-from kivy.properties import StringProperty
 
 
 class DynamicLabel(App):
-    status_text = StringProperty()
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.list_of_name = ['Jill', 'Dorothy', 'Gill']
@@ -20,11 +17,7 @@ class DynamicLabel(App):
     def create_widgets(self):
         for name in self.list_of_name:
             temp_button = Button(text=name)
-            temp_button.bind(on_release=self.press_entry)
             self.root.ids.entries_box.add_widget(temp_button)
-
-    def press_entry(self, instance):
-        self.status_text = instance.text
 
     def clear_all(self):
         self.root.ids.entries_box.clear_widgets()
